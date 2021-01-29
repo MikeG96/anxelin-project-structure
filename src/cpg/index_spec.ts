18e0a23e58c736ca983e93a49bb6011667180790
mikeag96 <miguelalopezg96@gmail.com>
2021-01-29 11:16:40 -0600

    test: add test for cpg schematic

---
+++ src/cpg/index_spec.ts
@@ -0,0 +1,18 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('cpg', () => {
+  it('works', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner
+      .runSchematicAsync('cpg', {}, Tree.empty())
+      .toPromise();
+
+    expect(tree.files).toEqual([
+      '/.prettierrc'
+    ]);
+  });
+});
