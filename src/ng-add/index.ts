0f1fef062d0c1c1c30d95359952474f3a8867a1f
mikeag96 <miguelalopezg96@gmail.com>
2021-01-28 18:55:29 -0600

    feat: add ng-add support

---
+++ src/ng-add/index.ts
@@ -0,0 +1,16 @@
+import {
+  chain,
+  Rule,
+  schematic,
+  SchematicContext,
+  Tree,
+} from '@angular-devkit/schematics';
+
+export default function (options: any): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    return chain([schematic('anxelin-project-structure', options)])(
+      host,
+      context
+    );
+  };
+}
